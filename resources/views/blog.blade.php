d3fd9b3c679478aca8612f9d581522afd21d5ef5
arifhyde98 <arif.hyde98@gmail.com>
2025-07-08 20:48:53 +0800

    rubah tailwind

--- resources/views/blog.blade.php
+++ resources/views/blog.blade.php
@@ -14,9 +14,23 @@
     <a href="/blog">blog</a>
     <a href="/contact">contact</a>
     <h1>ini adalah blog</h1>
-    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Blanditiis neque voluptas aut dolores saepe sit, at
-        deserunt fugiat maiores nobis rerum repellat rem possimus aspernatur molestias autem nesciunt delectus
-        distinctio.</p>
+
+    <article>
+        <h3>artikel 1</h3>
+        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Blanditiis neque voluptas aut dolores saepe sit, at
+            deserunt fugiat maiores nobis rerum repellat rem possimus aspernatur molestias autem nesciunt delectus
+            distinctio.</p>
+    </article>
+
+
+    <article>
+        <h3>artikel 2</h3>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum quisquam reprehenderit aperiam
+            explicabo
+            iste aliquid, tempore perferendis dolores libero obcaecati rerum, illo cumque sed vel consequatur vero
+            facere
+            est molestiae.</p>
+    </article>
 </body>
 
 </html>
--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+    <title>halaman kontak</title>
 </head>
 
 <body>
@@ -14,9 +14,11 @@
     <a href="/blog">blog</a>
     <a href="/contact">contact</a>
     <h1>ini adalah blog</h1>
-    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Blanditiis neque voluptas aut dolores saepe sit, at
-        deserunt fugiat maiores nobis rerum repellat rem possimus aspernatur molestias autem nesciunt delectus
-        distinctio.</p>
+    <ul>
+        <li>instagram : <a href="instagram.com">instagram</a></li>
+        <li>instagram : <a href="youtube.com">youtube</a></li>
+        <li>instagram : <a href="instagram.com">insta</a></li>
+    </ul>
 </body>
 
 </html>
